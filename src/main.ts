4269ad21212612d4f57ca528261868053e35146b
net <net@chamomind.local>
2025-10-01 12:59:56 +0700

    fest: add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+
+//Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {},
+})
 
 app.use(createPinia())
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vueDevTools(),
+    quasar({
+      autoImportComponentCase: 'pascal',
+      sassVariables: fileURLToPath(new URL('./src/css/quasar-variables.sass', import.meta.url)),
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
